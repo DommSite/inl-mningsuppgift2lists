c98cc6e37f25881e381eaffcccd19eaceae0c617
DommSite <johan.johansson06@live.se>
2024-02-02 12:34:51 +0100

    Add project files.

---
+++ inlämningsuppgift2lists/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace listinlämning_1
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            List<int> tal = new List<int>();
+            tal.Add(1);
+            tal.Add(2);
+            tal.Add(7);
+            tal.Add(3);
+            tal.Add(5);
+            tal.Add(1);
+            Max(tal);
+        }
+
+        static void Max(List<int>tal)
+        {
+            int störst = 0;
+            foreach (int item in tal)
+            {
+                if (item > störst) 
+                {
+                    störst = item;
+                }
+            }
+            Console.WriteLine(störst);
+        }
+
+    }
+}
